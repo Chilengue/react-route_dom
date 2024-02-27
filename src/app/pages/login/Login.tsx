cc91c6e4d32a8277bb7eec2543d923393cef6b1a
Chilengue <chilenguejoseorlando@gmail.com>
2024-02-27 09:30:23 +0200

    hookks

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,19 +1,48 @@
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 export const Login = () => {
     const [password, setPassword] = useState('');
      const [email, setEmail] = useState('');
 
-     const handleEntrar = ()=>{
+   //useMemo
+     const emailLenght =useMemo(()=>{
+        return email.length*12;
+    },[email.length]);
+    
+    // useEffect
+       useEffect(()=>{
+        if (window.confirm('voce e homem ')) {
+            console.log('homem'); }
+        else{
+            console.log('mulher'); } 
+    },[]);
+
+    useEffect(()=>{
         console.log(email); //recuperar valores que sao digitados pelo usuario
         console.log(password);
-        
-        
-     };
-        
+    },[email, password]); //este useEffect sera executado sempre que alteramos email e password
+
+
+    //  const handleEntrar = ()=>{
+    //     console.log(email); //recuperar valores que sao digitados pelo usuario
+    //     console.log(password);
+    //  };
+
+
+    //usecalbak ela devolve um void
+        const handleEntrar =useCallback(()=>
+        {
+            console.log(email);
+            console.log(password);
+            
+            
+        },[email, password])
+
         return (
               <div>
                 <form>
+                    <p>Quantidade de caractere de email :{emailLenght}</p>
+
                     <label>
                         <span>Email</span>
                         <input value={email} onChange={e => setEmail(e.target.value)} />
